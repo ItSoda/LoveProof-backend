3a316e9961ee3d24c9915c9fdcff89cdc34c9578
NN1798 <andrey.makarevich.03@mail.ru>
2024-06-19 23:04:08 +1000

    Add serialization for registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+
+from users.models import User
 
 
 class UserLoginSerializer(serializers.Serializer):
     """
     Сериализатор для входа пользователя.
-
-    Сериализует данные входа пользователя, включая электронную почту (email) и пароль (password).
 
     Поля:
     - email: поле для ввода электронной почты пользователя.
@@ -13,3 +15,56 @@ class UserLoginSerializer(serializers.Serializer):
     """
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для регистрации нового пользователя.
+
+    Поля:
+    - username: Имя пользователя.
+    - email: Адрес электронной почты пользователя (обязательное поле, должно быть уникальным).
+    - gender: Пол пользователя (обязательное поле).
+    - password: Пароль пользователя (обязательное поле).
+    - confirm_password: Подтверждение пароля (обязательное поле).
+
+    Валидация:
+    - Проверка совпадения полей `password` и `confirm_password`.
+
+    Создание пользователя:
+    - Удаление `confirm_password` из `validated_data`.
+    - Создание пользователя с использованием `create_user` метода модели `User`.
+    """
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'gender', 'password', 'confirm_password')
+        extra_kwargs = {
+            'gender': {'required': True}
+        }
+
+    def validate(self, attrs):
+        """
+        Проверка совпадения полей пароля.
+        """
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(
+            **validated_data,
+            is_active = False
+        )
+        return user
