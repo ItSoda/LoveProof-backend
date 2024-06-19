a52fcf4f1dccbaa163e1491c2b974d68fdaa20e8
NN1798 <andrey.makarevich.03@mail.ru>
2024-06-19 22:09:08 +1000

    Add route for logout

--- LoveProof/urls.py
+++ LoveProof/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from users.views import UserLoginAPIView
+from users.views import UserLoginAPIView, UserLogoutAPIView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/', UserLoginAPIView.as_view(), name='login'),
+    path('api/logout/', UserLogoutAPIView.as_view(), name='logout'),
 ]
