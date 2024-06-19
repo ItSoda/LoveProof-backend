9363a3eb405bc208b559151cf6b99e472c8035a9
NN1798 <andrey.makarevich.03@mail.ru>
2024-06-19 21:43:46 +1000

    Add route for login

--- LoveProof/urls.py
+++ LoveProof/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from users.views import UserLoginAPIView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/login/', UserLoginAPIView.as_view(), name='login'),
 ]
