505c9b8fb09138b3e99643638c5b3fba69308cdc
NN1798 <andrey.makarevich.03@mail.ru>
2024-06-26 04:39:30 +1000

    Add new fields to the User model

--- users/models.py
+++ users/models.py
@@ -30,6 +30,9 @@ class User(AbstractUser):
     description = models.TextField(verbose_name='Описание')
     age = models.PositiveSmallIntegerField(null=True, validators=[MinValueValidator(18)], verbose_name='Возраст')
     tags = models.ManyToManyField(to='Tag', related_name='users', verbose_name='Тег')
+    location = models.ForeignKey(to='Location', on_delete=models.SET_NULL, null=True, related_name='users', verbose_name='Локация')
+    ethnicity = models.ForeignKey(to='Ethnicity', on_delete=models.SET_NULL, null=True, related_name='users', verbose_name='Этничность')
+    social_media = models.ManyToManyField(to='SocialMedia', related_name='users', verbose_name='Социальная сеть')
     role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='client', verbose_name='Роль')
     price = models.PositiveSmallIntegerField(default=0, verbose_name='Цена')
     email_confirmed = models.BooleanField(default=False, verbose_name='Email подтвержден')
@@ -71,6 +74,36 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Location(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Ethnicity(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Этничность'
+        verbose_name_plural = 'Этничности'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class SocialMedia(models.Model):
+    title = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Социальная сеть'
+        verbose_name_plural = 'Социальные сети'
 
 
 class EmailVerification(models.Model):
