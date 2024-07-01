dbd27238e52bb83cfef279833e7e672cf57b7228
NN1798 <andrey.makarevich.03@mail.ru>
2024-07-02 01:43:56 +1000

    Add models to the admin panel

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from community.models import Post, Comment, Like, Category
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'header', 'category', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('header', 'text')
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('text',)
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at',)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'comment', 'created_at')
+    list_filter = ('created_at',)
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title',)
