f2156ef0f0d7e1a484f4272851bd19209793bc85
NN1798 <andrey.makarevich.03@mail.ru>
2024-07-02 04:58:56 +1000

    Add a route and its handler PostListCreateAPIView

---
+++ community/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from community.views import PostListCreateAPIView, PostDetailAPIView, CommentListCreateAPIView, CommentDetailView, LikePostAPIView, LikeCommentAPIView
+
+
+app_name = 'community'
+
+urlpatterns = [
+    path('', PostListCreateAPIView.as_view(), name='post_list_create'),
+    path('<int:pk>/detail/', PostDetailAPIView.as_view(), name='post_detail'),
+    path('posts/<int:post_pk>/comments/', CommentListCreateAPIView.as_view(), name='comment_list_create'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment_detail'),
+    path('posts/<int:post_pk>/like/', LikePostAPIView.as_view(), name='like_post'),
+    path('comments/<int:comment_pk>/like/', LikeCommentAPIView.as_view(), name='like_comment'),
+]
--- community/views.py
+++ community/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from community.serializers import PostSerializer
+from community.services.views_service import (
+    get_list_post
+)
+from community.pagination import PostPagination
+
+
+class PostListCreateAPIView(APIView):
+    """
+    API view для выдачи списка постов и создания новых.
+
+    Пользователь должен быть аутентифицирован для доступа к данным постов.
+
+    Параметры запроса:  
+    {   
+        "header": "Заголовок поста",
+        "text": "Текст поста",
+        "category": "id категории (необязательно)"
+    } 
+
+    HTTP коды ответа:
+    - GET:
+        - 200 OK: Возвращает список всех постов.
+        - 400 Bad Request: Ошибка в запросе или неверные данные.
+    - POST:
+        - 201 Created: Пост успешно создан.
+        - 400 Bad Request: Ошибка в запросе или неверные данные для создания поста.
+    """
+    permission_classes = [IsAuthenticated]
+    pagination_class = PostPagination
+
+    def get(self, request):
+        posts = get_list_post()
+
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(posts, request)
+        if page is not None:
+            serializer = PostSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
