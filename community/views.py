d33b2cfc861c7c9a28b57946798493b2b3b014c0
NN1798 <andrey.makarevich.03@mail.ru>
2024-07-02 05:02:45 +1000

    Add a "pk/detail" route and its handler PostDetailAPIView

--- community/views.py
+++ community/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
-from community.serializers import PostSerializer
+from community.serializers import PostSerializer, PostDetailSerializer
+from community.permissions import IsOwnerOrReadOnly, IsModeratorOrAdmin
 from community.services.views_service import (
-    get_list_post
+    get_list_post, get_post,
 )
 from community.pagination import PostPagination
 
@@ -52,3 +53,52 @@ class PostListCreateAPIView(APIView):
             serializer.save(user=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetailAPIView(APIView):
+    """
+    API view для детального просмотра, обновления и удаления поста.
+
+    Пользователь должен быть аутентифицирован и иметь права доступа для изменения данных поста.
+
+    Параметры запроса:
+    - PUT
+        {
+            "header": "Новый заголовок",
+            "text": "Новый текст",
+            "category": "Новая категория (необязательно)"
+        }  # Можно изменить 1 поле на выбор
+
+    HTTP коды ответа:
+    - GET:
+        - 200 OK: Возвращает данные конкретного поста.
+        - 404 Not Found: Пост не найден.
+    - PUT:
+        - 200 OK: Данные поста успешно обновлены.
+        - 400 Bad Request: Ошибка в запросе или неверные данные для обновления поста.
+        - 403 Forbidden: Пользователь не имеет прав доступа к изменению данных поста.
+    - DELETE:
+        - 204 No Content: Пост успешно удален.
+        - 403 Forbidden: Пользователь не имеет прав доступа к удалению поста.
+    """
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly, IsModeratorOrAdmin]
+
+    def get(self, request, pk):
+        post = get_post(pk)
+        serializer = PostDetailSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = get_post(pk)
+        self.check_object_permissions(request, post)
+        serializer = PostDetailSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = get_post(pk)
+        self.check_object_permissions(request, post)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
