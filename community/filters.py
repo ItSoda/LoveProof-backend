34509efbfde8796dedcd195e33fefcb0f6206374
NN1798 <andrey.makarevich.03@mail.ru>
2024-07-03 01:48:37 +1000

    Add a get_filtered_queryset function for filtering

--- community/filters.py
+++ community/filters.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from django.db.models.query import QuerySet
+from django.http import QueryDict
 import django_filters
 
 from community.models import Post
@@ -17,3 +21,27 @@ class PostFilter(django_filters.FilterSet):
     class Meta:
         model = Post
         fields = ('category',)
+
+
+def get_filtered_queryset(queryset: QuerySet, query_params: QueryDict, filterset_class: django_filters.FilterSet) -> Optional[QuerySet]:
+    """
+    Функция для фильтрации queryset на основе переданных параметров.
+
+    Параметры:
+        queryset (QuerySet): Исходный QuerySet, который требуется отфильтровать.
+        query_params (QueryDict): Параметры запроса, используемые для фильтрации.
+        filterset_class (FilterSet): Класс фильтра Django, который определяет, какие параметры использовать.
+
+    Возвращает:
+        Optional[QuerySet]: Отфильтрованный QuerySet. Если фильтрация не привела к результатам, возвращается None.
+    """
+    print(query_params)
+    if any(query_params.values()):  # Проверка фильтрации в запросе
+        filterset = filterset_class(query_params, queryset=queryset)
+        filtered_queryset = filterset.qs
+        if not filtered_queryset.exists():
+            return None  # Возвращается None, если результат фильтрации пуст
+    else:
+        filtered_queryset = queryset  # Если нет фильтров, используется исходный queryset
+
+    return filtered_queryset
