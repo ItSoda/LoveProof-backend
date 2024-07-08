9bf6722b464d563bd8c8c08eab1039ab5f5aa71b
NN1798 <andrey.makarevich.03@mail.ru>
2024-07-08 23:13:48 +1000

    Add a route for the "chat" app

--- LoveProof/urls.py
+++ LoveProof/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 
 from users.views import UserRegistrationView, UserLoginAPIView, UserLogoutAPIView, VerifyEmailAPIView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/register/', UserRegistrationView.as_view(), name='register'),
     path('api/login/', UserLoginAPIView.as_view(), name='login'),
     path('api/logout/', UserLogoutAPIView.as_view(), name='logout'),
     path('verify-email/', VerifyEmailAPIView.as_view(), name='email_verify'),
-    path('api/v1/users/', include('users.urls', namespace='users'))
+    path('api/v1/users/', include('users.urls', namespace='users')),
+    path('api/v1/chats/', include('chat.urls', namespace='chat')),
 ]
